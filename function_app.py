4ec31e73b2943bf65ff8b0ec4e95a644b3e6db3a
Aidar Darmesh <darmesh.aidar@gmail.com>
2025-08-19 19:17:06 +0500

    Fixed db and container names

--- function_app.py
+++ function_app.py
@@ -3,7 +3,11 @@ import logging
 
 app = func.FunctionApp()
 
-@app.cosmos_db_trigger(arg_name="azcosmosdb", container_name="adnoc-gpt",
-                        database_name="agents", connection="azuren8ntutorial_DOCUMENTDB")  
+@app.cosmos_db_trigger(arg_name="azcosmosdb", 
+                        container_name="agents",
+                        database_name="adnoc-gpt",
+                        connection="azuren8ntutorial_DOCUMENTDB",
+                        lease_container_name="leases",
+                        create_lease_container_if_not_exists=True)
 def cosmosdb_trigger(azcosmosdb: func.DocumentList):
     logging.info('Python CosmosDB triggered.')
