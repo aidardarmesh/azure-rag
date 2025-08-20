4cec07cccbf6e831d2f3d6d9b3bbce985dcd0e5e
Aidar Darmesh <darmesh.aidar@gmail.com>
2025-08-20 11:12:51 +0500

    Binded SignalR Service into Azure Function App

--- function_app.py
+++ function_app.py
@@ -1,20 +1,38 @@
 import azure.functions as func
+from azure.functions import SignalRMessage
 import logging
-from pprint import pprint
 
 app = func.FunctionApp()
 
-@app.cosmos_db_trigger(arg_name="azcosmosdb", 
-                        container_name="agents",
-                        database_name="adnoc-gpt",
-                        connection="azuren8ntutorial_DOCUMENTDB",
-                        lease_container_name="leases",
-                        create_lease_container_if_not_exists=True)
-def cosmosdb_trigger(azcosmosdb: func.DocumentList):
+@app.cosmos_db_trigger(
+    arg_name="azcosmosdb",
+    container_name="agents",
+    database_name="adnoc-gpt",
+    connection="azuren8ntutorial_DOCUMENTDB",
+    lease_container_name="leases",
+    create_lease_container_if_not_exists=True)
+@app.signalr_output(
+    arg_name="signalr_messages",
+    hub_name="n8nHub",
+    connection="AzureSignalRConnectionString"
+)
+def cosmosdb_trigger(
+    azcosmosdb: func.DocumentList,
+    signalr_messages: func.Out[func.SignalRMessage]
+):
     if azcosmosdb:
-        logging.info("Changes detected in CosmosDB 'agents' container.")
+        logging.info(f"{len(azcosmosdb)} document changes detected.")
+        messages = []
         for doc in azcosmosdb:
-            # Log the full document received by the trigger
-            logging.info(f"Document received: {doc.to_json()}")
+            # Send each changed document as a SignalR message to all clients
+            message = SignalRMessage(
+                target="newAgentUpdate",
+                arguments=[doc.to_json()]
+            )
+            messages.append(message)
+
+        signalr_messages.set(messages)
     else:
-        logging.info("No documents received from the trigger.")
+        logging.info("No document changes detected.")
+
+
